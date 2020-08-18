2c037aead1e1eff75056ad89a6fa87b2bb49407e
Pasquale Scalise <pasqualescalise0@gmail.com>
2020-08-18 18:34:15 +0200

    Aggiunto RollingBack

--- generatore.py
+++ generatore.py
@@ -7,10 +7,6 @@ linee di testo che l'editor produce, a prescindere dalla struttura
 dati utilizzata
 '''
 
-# Costanti
-MAXLEN = 1024
-letters = ['c', 'd', 'p', 'u', 'r']
-
 '''
 Maxfilelen è una lista che tiene traccia continuamente della
 lunghezza del file, in modo da avere un riferimento da usare
@@ -24,8 +20,23 @@ maxfilelen per sapere qual è la dimensione del file attuale
 '''
 index = 0
 
+
+'''
+INPUT
+'''
+
+# Lunghezza massima del file
+maxlen = int(input("Massima lughezza del file? [default 1024] ") or "1024")
+
 # Numero di comandi del test
-length = int(input("Quanti comandi? "))
+length = int(input("Quanti comandi? [default 1000] ") or "1000")
+
+# Categoria di test
+category = int(input("Che modalità di test?\n\
+        0. Test con tutti i comandi possibili, in ordine casuale\n\
+        1. Test simile a RollingBack, c, d e p all'inizio, u, r e p alla fine (ultimo 10%)\n\
+        Scegliere il numero corrispondente [default 0] ") or "0")
+
 
 # Crea un array con le frasi da usare nelle Change
 quotes = []
@@ -37,15 +48,26 @@ f.close()
 # Crea il test
 f = open("test.txt", "w")
 
+letters = ['c', 'p', 'd', 'u', 'r']
+letters1 = ['c', 'p', 'd']
+letters2 = ['u', 'r', 'p']
+
 # Ciclo principale
+tenpercent = length - (length * (1/10))
 for i in range(length - 1):
     # sceglie a caso una lettera tra le 5
-    letter = choice(letters)
+    if category == 0:
+        letter = choice(letters)
+    else:
+        if i < tenpercent:
+            letter = choice(letters1)
+        else:
+            letter = choice(letters2)
 
     # Undo o Redo
     if letter == 'u' or letter == 'r':
-        # sceglie un numero a caso tra 0 e 1024
-        num = choice(range(MAXLEN))
+        # sceglie un numero a caso tra 0 e maxlen
+        num = choice(range(maxlen))
         # crea il comando e lo mette nella stringa s
         s = '{}{}'.format(num, letter)
 
@@ -62,10 +84,10 @@ for i in range(length - 1):
 
     # Delete o Print
     elif letter == 'd' or letter == 'p':
-        # sceglie due numeri a caso tra 0 e 1024, dove il secondo
+        # sceglie due numeri a caso tra 0 e maxlen, dove il secondo
         # è maggiore del primo
-        num0 = choice(range(MAXLEN))
-        num1 = choice(range(num0, MAXLEN))
+        num0 = choice(range(maxlen))
+        num1 = choice(range(num0, maxlen))
         # crea il comando e lo mette nella stringa s
         s = '{},{}{}'.format(num0, num1, letter)
 
@@ -101,7 +123,7 @@ for i in range(length - 1):
         # altrimenti, numero casuale tra 1 e la lunghezza attuale + 1
         else:
             num0 = choice(range(1, maxfilelen[index] + 1))
-        num1 = choice(range(num0, MAXLEN))
+        num1 = choice(range(num0, maxlen))
 
         # crea il comando e lo mette nella stringa s
         s = '{},{}{}\n'.format(num0, num1, letter)
